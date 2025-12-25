34214ff736a1ae11792e71113ce3d5c9140918b5
Anastasia Markina <ahmarkina@gmail.com>
2025-12-25 18:14:59 +0200

    Updated versions

--- src/main/java/utils/RestHelper.java
+++ src/main/java/utils/RestHelper.java
@@ -4,8 +4,8 @@ import io.restassured.response.ValidatableResponse;
 import static io.restassured.RestAssured.given;
 
 public class RestHelper {
-    public static ValidatableResponse get(String endpoint) {
-        String baseUrl = "http://app:" + OrchestratorHelper.getDynamicPort("app");  // Из оркестратора
-        return given().baseUri(baseUrl).when().get(endpoint).then();
-    }
+//    public static ValidatableResponse get(String endpoint) {
+//        String baseUrl = "http://app:" + OrchestratorHelper.getDynamicPort("app");  // Из оркестратора
+//        return given().baseUri(baseUrl).when().get(endpoint).then();
+//    }
 }
